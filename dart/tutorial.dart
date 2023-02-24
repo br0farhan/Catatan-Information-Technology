a2918443dffa4158a7560420728cda8aec39533f
Farhan Martiyansyah <far.martiyansyah@gmail.com>
2023-02-24 16:12:59 +0700

    commit tutorial dart

---
+++ dart/tutorial.dart
@@ -0,0 +1,11 @@
+void main() {
+  //tipe_data nama_variabel = nilai_awal
+  String myname = "Farhan Martiyansyah";
+  String age = "26";
+  double price = 20.50;
+  //konver data bisa menggunakan "parse"
+  int konver = int.parse(age);
+
+  print("$myname");
+  print("umur saya $age");
+}
