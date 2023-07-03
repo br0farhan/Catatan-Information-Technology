369e1f12fc2b3c27b461dfc6d3558c48e9c5daa3
Farhan Martiyansyah <far.martiyansyah@gmail.com>
2023-07-03 17:11:09 +0700

    Tipe List & Map

---
+++ dart/basic/main.dart
@@ -0,0 +1,28 @@
+//LIST & MAP
+
+// void main(List<String> arguments) {
+
+    //Bisa menyimpa nilai lebih dari 1
+    //Penyimpanan berbasi index
+    // List products = [
+    // "Malboro", // 0
+    // "Sampoerna Mild", // 1
+    // "Dji Sam Soe", // 2
+    // "Sampoerna Kreteg", // 3
+    // ];
+
+  // print(products[0]);
+  //Bisa menyimpa nilai lebih dari 1
+  //Penyimpanan berbasi key dan value
+  // Map product = {
+    //Key : Value
+      // "Roko Mas Cahyo"    : "Malboro",
+      // "Roko Mas Agis"     : "Sampoerna Mild",
+      // "Roko Mas Adrian"   : "Dji Sam Soe",
+      // "Roko Mas Yogi"     : "Sampoerna Kreteg"
+  // };
+  // print(product["Roko Mas Yogi"]);
+// }
+
+
+//KONVERSI TIPE DATA
